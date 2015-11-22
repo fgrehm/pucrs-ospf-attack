0a870aefe86eda94fd6b3d232b7c2ba17587c727
Pedro de Bem <pedro@debian>
2015-11-22 20:18:00 -0200

    Create ospf.c and ospf.h with we finded documents in site www.cs.unc.edu, I created structs and methods to work, my questions I writed in code comment preceded of ?pedro or wireshark words.

---
+++ ospf.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+
+#include "ospf.h"
+
+
+//?pedro                                               //verificar se tipo da variável é adequada
+int build_link_state_summary_header_ospf(unsigned char buffer[BUFFER_LEN], 
+                                    char *local_ip, 
+                                    char *dest_ip, 
+                                    int size_eth_ip_ospfBasic, 
+                                    int tipe_of_service,
+                                    unsigned long sequence_number) {
+
+  // OSPF link state summary header
+  // possibly necessary a list of this ?pedro
+  struct ospf_lss *lss_header_ospf;
+  lss_header_ospf = (struct ospf_lss *) &buffer[size_eth_ip_ospfBasic];
+  lss_header_ospf->lss_age = LSS_AGE;                                       /* Time (secs) Since Originated ?pedro I can't know if fixed value */
+  lss_header_ospf->lss_opts = LSS_OPTIONS;                                  /* Options Supported */
+  lss_header_ospf->lss_type = LSST_ROUTE;                                   /* LST_* below ?pedro */
+  lss_header_ospf->lss_lsid = inet_addr(local_ip);                          /* Link State Identifier */
+  lss_header_ospf->lss_rid = inet_addr(local_ip);                           /* Advertising Router Identifier ?pedro I think would was THE PHANTOM ROUTER*/
+  lss_header_ospf->lss_seq = LSS_SEQ_NUM;                                   /* Link State Adv. Sequence #   */
+    // TODO: CHECKSUM: lss_header_ospf->lss_cksum;  /* ?pedro Fletcher Checksum of LSA */
+    // TODO: LENGTH: lss_header_ospf->lss_len;    /* Length of Advertisement ?pedro I don't know, because in wireshark a header has 3 LSS and values not equal*/
+};
+
+  return sizeof(struct ospf_lss);
+}
+
+//?pedro                                               //verificar se tipo da variável é adequada
+int build_link_state_request_header_ospf(unsigned char buffer[BUFFER_LEN], 
+                                    char *local_ip, 
+                                    char *dest_ip, 
+                                    int size_eth_ip_ospfBasic, 
+                                    int tipe_of_service,
+                                    unsigned long sequence_number) {
+
+  // OSPF link state request header
+  // possibly necessary a list of this ?pedro
+  struct ospf_lsr *lsr_header_ospf;
+  lsr_header_ospf = (struct ospf_lsr *) &buffer[size_eth_ip_ospfBasic];
+  lsr_header_ospf->lsr_type = LSR_TYPE;                                     /* Link State Type      */
+  lsr_header_ospf->lsr_lsid = inet_addr(dest_ip);                           /* Link State Identifier "destination ip address" */
+  lsr_header_ospf->lsr_rid = inet_addr(dest_ip);                            /* Advertising Router "destination ip address" */
+
+  return sizeof(struct ospf_lsr);
+}
+
+//?pedro                                               //verificar se tipo da variável é adequada
+int build_database_description_header_ospf(unsigned char buffer[BUFFER_LEN], 
+                                    char *local_ip, 
+                                    char *dest_ip, 
+                                    int size_eth_ip_ospfBasic, 
+                                    int tipe_of_service,
+                                    unsigned long sequence_number) {
+
+  // OSPF data base description header
+  struct ospf_dd *database_description_header_ospf;
+  database_description_header_ospf = (struct ospf_dd *) &buffer[size_eth_ip_ospfBasic];
+  database_description_header_ospf->dd_mbz = ZERO;                          /* Must Be Zero         */
+  database_description_header_ospf->dd_opts = DD_OPTIONS;                   /* Options          */
+  // TODO: CONTROL: database_description_header_ospf->dd_control; /* Control Bits (DDC_* below)   */
+  database_description_header_ospf->dd_seq = sequence_number;               /* Sequence Number      */
+  // TODO: LINK STATE ADVERTISEMENTS LIST: struct ospf_lss dd_lss[1];  /* Link State Advertisements    */
+  
+  return sizeof(struct ospf_dd);
+}
+
+//?pedro                                     //verificar se tipo da variável é adequada
+int build_hello_header_ospf(unsigned char buffer[BUFFER_LEN], 
+                                    char *local_ip, 
+                                    char *dest_ip, 
+                                    int size_eth_ip_ospfBasic, 
+                                    int tipe_of_service) {
+
+  // OSPF hello header
+  struct ospf_hello *hello_header_ospf;
+  hello_header_ospf = (struct ospf_hello *) &buffer[size_eth_ip_ospfBasic];
+  hello_header_ospf->oh_netmask = inet_addr("255.255.255.0");               /* Network Mask     */
+  hello_header_ospf->oh_hintv = HELLO_INTERVAL;                             /* Hello Interval (seconds) */
+  /*OPTIONS DON'T OK*/
+  hello_header_ospf->oh_opts = HELLO_OPTIONS;                               /* Options      */
+  hello_header_ospf->oh_prio = HELLO_PRIORITY;                              /* Sender's Router Priority */
+  hello_header_ospf->oh_rdintv = HELLO_DEAD_INTERVAL;                       /* Seconds Before Declare Dead  */
+  hello_header_ospf->oh_drid = inet_addr(local_ip);                         /* Designated Router ID   */
+  hello_header_ospf->oh_brid = inet_addr("0.0.0.0");                        /* Backup Designated Router ID  */
+  // TODO: NEIGHBOR LIST: hello_header_ospf->oh_neighbor[1];                                        /* Living Neighbors   */
+  
+  return sizeof(struct ospf_hello);
+}
+
+//?pedro                                    //verificar se tipo da variável é adequada
+int build_basic_header_ospf(unsigned char buffer[BUFFER_LEN], 
+                                    char *local_ip,
+                                    char *dest_ip,
+                                    int size_eth_ip,
+                                    int tipe_of_service) {
+
+  // OSPF header
+  struct ospf *basic_header_ospf;
+  basic_header_ospf = (struct ospf *) &buffer[size_eth_ip];
+  basic_header_ospf->ospf_version = OSPF_VERSION;               /* Version Number   */
+  basic_header_ospf->ospf_type = tipe_of_service;               /* Packet Type      */
+  //TODO: OSPF_LENTH: basic_header_ospf->ospf_len = ; /* Packet Length    */
+  basic_header_ospf->ospf_rid = inet_addr(local_ip);            /* Router Identifier    */
+  basic_header_ospf->ospf_aid = inet_addr("0.0.0.0");           /* Area Identifier    */
+  // TODO: CHECKSUM:  basic_header_ospf->ospf_cksum = ;         /* Check Sum      */
+  basic_header_ospf->ospf_authtype = ;                          /* Authentication Type    */
+  ospf_auth[AUTHLEN] = AU_NONE;                                 /* Authentication Field */
+  basic_header_ospf->ospf_data[1];
+
+  return sizeof(struct ospf);
+}
---
+++ ospf.h
@@ -0,0 +1,195 @@
+/* OSPF packet format */
+
+struct ospf {
+	unsigned char	ospf_version;	/* Version Number		*/
+	unsigned char	ospf_type;	/* Packet Type			*/
+	unsigned short	ospf_len;	/* Packet Length		*/
+	unsigned long	ospf_rid;	/* Router Identifier		*/
+	unsigned long	ospf_aid;	/* Area Identifier		*/
+	unsigned short	ospf_cksum;	/* Check Sum			*/
+	unsigned short	ospf_authtype;	/* Authentication Type		*/
+	unsigned char	ospf_auth[AUTHLEN]; /* Authentication Field	*/
+	unsigned char	ospf_data[1];
+};
+
+#define OSPF_VERSION 2		/**/
+#define	MINHDRLEN	24		/* OSPF base header length	*/
+
+/* OSPF Packet Types */
+
+#define	T_HELLO		1		/* Hello packet			*/
+#define	T_DATADESC	2		/* Database Description		*/
+#define	T_LSREQ		3		/* Link State Request		*/
+#define	T_LSUPDATE	4		/* Link State Update		*/
+#define	T_LSACK		5		/* Link State Acknowledgement	*/
+
+/* OSPF Authentication Types */
+
+#define	AU_NONE		0		/* No Authentication		*/
+#define	AU_PASSWD	1		/* Simple Password		*/
+
+/* OSPF Hello Packet */
+
+struct	ospf_hello {
+	unsigned long	oh_netmask;	/* Network Mask			*/
+	unsigned short	oh_hintv;	/* Hello Interval (seconds)	*/
+	unsigned char	oh_opts;	/* Options			*/
+	unsigned char	oh_prio;	/* Sender's Router Priority	*/
+	unsigned long	oh_rdintv;	/* Seconds Before Declare Dead	*/
+	unsigned long	oh_drid;	/* Designated Router ID		*/
+	unsigned long	oh_brid;	/* Backup Designated Router ID	*/
+	unsigned long	oh_neighbor[1];	/* Living Neighbors		*/
+};
+
+#define	HELLO_INTERVAL	10				/* 10 seconts defined */
+#define	HELLO_OPTIONS	0X12			/* Take default options from wireshark message */
+#define	HELLO_PRIORITY	1				/* Take default priority from wireshark message */
+#define	HELLO_DEAD_INTERVAL	0X40		/* Take default dead interval from wireshark message */
+#define	MINHELLOLEN	(MINHDRLEN + 20)
+
+/* OSPF Database Description Packet */
+
+struct	ospf_dd {
+	unsigned short	dd_mbz;		/* Must Be Zero			*/
+	unsigned char	dd_opts;	/* Options			*/
+	unsigned char	dd_control;	/* Control Bits	(DDC_* below)	*/
+	unsigned long	dd_seq;		/* Sequence Number		*/
+	struct ospf_lss	dd_lss[1];	/* Link State Advertisements	*/
+};
+
+#define	MINDDLEN	(MINHDRLEN + 8)
+
+#define ZERO 		0X00 		/* Must Be Zero */
+#define DD_OPTIONS	0X52		/* Take default options from wireshark message */
+#define	DDC_INIT	0x04		/* Initial Sequence		*/
+#define	DDC_MORE	0x02		/* More to follow		*/
+#define	DDC_MSTR	0x01		/* This Router is Master	*/
+
+/* second page */
+
+/* OSPF Link State Request Packet */
+
+struct	ospf_lsr {
+	unsigned long	lsr_type;	/* Link State Type		*/
+	unsigned long	lsr_lsid;	/* Link State Identifier	*/
+	unsigned long	lsr_rid;	/* Advertising Router		*/
+};
+
+#define LSR_TYPE 	0X00000001	/* Take default type from wireshark message */
+#define	LSRLEN		12
+
+/* OSPF Link State Summary */
+
+struct	ospf_lss {
+	unsigned short	lss_age;	/* Time (secs) Since Originated	*/
+	unsigned char	lss_opts;	/* Options Supported		*/
+	unsigned char	lss_type;	/* LST_* below			*/
+	unsigned long	lss_lsid;	/* Link State Identifier	*/
+	unsigned long	lss_rid;	/* Advertising Router Identifier*/
+	unsigned long	lss_seq;	/* Link State Adv. Sequence #	*/
+	unsigned short	lss_cksum;	/* Fletcher Checksum of LSA	*/
+	unsigned short	lss_len;	/* Length of Advertisement	*/
+};
+
+#define	LSSHDRLEN	20
+#define LSS_AGE 	144			/* Take default age from wireshark message */
+#define LSS_OPTIONS 0X22		/* Take default age from wireshark message */
+#define LSST_ROUTE	0X01		/* Link to a router :: Take default LS Type from wireshark message */
+#define LSST_NET	0X02		/* Link to a network */
+#define LSST_SUM_IP	0X03		/* When area are used, summary information generaled about a network */
+#define LSST_SUMLSA	0X04		/* When area are used, summary information about a link to an AS boundary router*/
+#define LSST_AS_EXT	0X05		/* An external link outside the autonomous system */
+#define LSS_SEQ_NUM	0x80000001	/* Take default sequence number from wireshark message */
+
+/* Link State Advertisement Types */
+
+#define	LST_RLINK	1		/* Router Link			*/
+#define	LST_NLINK	2		/* Network Link			*/
+#define	LST_SLINK	3		/* IP Network Summary Link	*/
+#define	LST_BRSLINK	4		/* AS Border Router Summary	*/
+#define	LST_EXTERN	5		/* AS External Link		*/
+
+/* Link State Advertisement (min) Lengths */
+
+#define	LSA_RLEN	(LSSHDRLEN + 4)
+#define	LSA_NLEN	(LSSHDRLEN + 4)
+
+#define	LSA_ISEQ	0x80000001
+
+
+/* text commented because the program don't need this */
+
+/* LSS Type of Service Entry */
+//
+//	struct	tosent {
+//		unsigned char	tos_tos;	/* IP Type of Service		*/
+//		unsigned char	tos_mbz;	/* Must Be Zero			*/
+//		unsigned short	tos_metric;	/* Metric for This TOS		*/
+//	};
+
+/* OSPF Link State Advertisement */
+
+#define	MAXLSDLEN	64	/* Max LS Data Len (configurable)	*/
+
+struct ospf_lsa {
+	struct ospf_lss	lsa_lss;	/* Link State Adv. Header	*/
+	char		lsa_data[MAXLSDLEN]; /* Link-Type Dependent Data*/
+};
+
+/* Convenient Field Translations */
+
+#define	lsa_age		lsa_lss.lss_age
+#define	lsa_opts	lsa_lss.lss_opts
+#define	lsa_type	lsa_lss.lss_type
+#define	lsa_lsid	lsa_lss.lss_lsid
+#define	lsa_rid		lsa_lss.lss_rid
+#define	lsa_seq		lsa_lss.lss_seq
+#define	lsa_cksum	lsa_lss.lss_cksum
+#define	lsa_len		lsa_lss.lss_len
+
+/* text commented because the program don't need this */
+
+/* Router Links Advertisement */
+//
+//	struct	ospf_ra {
+//		unsigned char	ra_opts;	/* RAO_* Below			*/
+//		unsigned char	ra_mbz;		/* Must Be Zero			*/
+//		unsigned short	ra_nlinks;	/* # of Links This Advertisement*/
+//		unsigned char	ra_data[1];	/* nlinks rlink structs		*/
+//	};
+//
+//	struct ospf_rl {
+//		unsigned long	rl_lid;		/* Link ID			*/
+//		unsigned long	rl_data;	/* Link Data			*/
+//		unsigned char	rl_type;	/* Link Type (RAT_* Below)	*/
+//		unsigned char	rl_ntos;	/* # of Types-of-Service Entries*/
+//		unsigned short	rl_metric;	/* TOS 0 Metric			*/
+//		unsigned long	rl_tosent[1];	/* TOS Entries ra_ntos Times	*/
+//	};
+
+#define	MINRLLEN	12
+
+#define	RAO_ABR		0x01		/* Router is Area Border Router	*/
+#define	RAO_EXTERN	0x02		/* Router is AS Boundary Router	*/
+
+#define	RAT_PT2PT	1		/* Point-Point Connection	*/
+#define	RAT_TRANSIT	2		/* Connection to Transit Network*/
+#define	RAT_STUB	3		/* Connection to Stub Network	*/
+#define	RAT_VIRTUAL	4		/* Virtual Link			*/
+
+/* Network Links Advertisement */
+
+struct	ospf_na {
+	IPaddr		na_mask;	/* Network Mask			*/
+	unsigned long	na_rid[1];	/* IDs of All Attached Routers	*/
+};
+
+/* Link State Update Packet Format */
+
+struct	ospf_lsu {
+	unsigned long	lsu_nads;	/* # Advertisments This Packet	*/
+	char		lsu_data[1];	/* 1 or more struct ospf_lsa's	*/
+};
+
+#define	MINLSULEN	(MINHDRLEN + 4)	/* Base LSU Length		*/
+
